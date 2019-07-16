ea124a3c17a072aea16576a6117c4d813216e233
36927387@qq.com <369237387@qq.com>
2019-07-16 20:44:24 +0800

    创建项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,72 @@
+module.exports = {
+  title:"xuks",
+  description: 'Welcome to my personal website',
+  dest: './docs/dist',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig: {
+    type: 'blog',
+    huawei: false,
+    nav: [
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: 'TimeLine', link: '/timeLine/', icon: 'reco-date' },
+      { text: 'Contact', 
+        icon: 'reco-message',
+        items: [
+          { text: 'GitHub', link: 'https://github.com/xukaishan', icon: 'reco-github' },
+          { text: '简书', link: 'https://www.jianshu.com/u/a6ccd6700041', icon: 'reco-jianshu' },
+          { text: 'CSDN', link: 'https://blog.csdn.net/qq_43594899', icon: 'reco-csdn' },
+          { text: '博客园', link: 'https://home.cnblogs.com/u/1522663/', icon: 'reco-bokeyuan' },
+        ]
+      }
+    ],
+    // 博客设置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认 “分类”
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag' // 默认 “标签”
+      }
+    },
+    logo: '/head.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    sidebar: 'auto',
+    // 最后更新时间
+    lastUpdated: 'Last Updated',
+    // 作者
+    author: '',
+    // 项目开始时间
+    startYear: '2018'
+    /**
+     * 密钥 (if your blog is private)
+     */
+
+    // keyPage: {
+    //   keys: ['your password'],
+    //   color: '#42b983',
+    //   lineColor: '#42b983'
+    // },
+
+    /**
+     * valine 设置 (if you need valine comment )
+     */
+
+    // valineConfig: {
+    //   appId: '...',// your appId
+    //   appKey: '...', // your appKey
+    // }
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: ['@vuepress/medium-zoom', 'flowchart']
+}  
